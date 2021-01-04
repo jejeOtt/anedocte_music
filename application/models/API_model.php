ceb6cbb259d04d1b08d5e20dab45e07b857302ee
jejeOtt <48128613+jejeOtt@users.noreply.github.com>
2021-01-04 16:21:39 +0100

    Story and importForm

--- application/models/API_model.php
+++ application/models/API_model.php
@@ -6,13 +6,13 @@ class API_model extends CI_Model
         $this->load->database();
     }
 
-    public function get_API($slug = FALSE, $genreName)
+    public function get_API($slug = FALSE, $genreName, $storyName)
     {
 
         $slug = url_title($genreName);
         $data = array(
             'genreName' => $genreName,
-            'story' => $this->input->post('story'),
+            'story' => $storyName,
             'idUser' => $this->session->userdata['idUser'],
             'isValidated' => 1,
             'slug' => $slug,
--- application/views/admin_office/import.php
+++ application/views/admin_office/import.php
@@ -4,8 +4,8 @@
 
 <?php echo form_open('admin_office/import'); ?>
   <div class="form-group">
-    <label>Choisissez le nombre de genre que vous voulez importer</label>
-    <input type="number" class="form-control" name="importForm" placeholder="Ajoutez un nombre">
+    <label>Choisissez le nombre de genre et de stories que vous voulez importer</label>
+    <input type="number" min="1" max="100" class="form-control" name="importForm" placeholder="Ajoutez un nombre">
   </div>
   <button type="submit" class="btn btn-primary">Validé</button>
 </form>
