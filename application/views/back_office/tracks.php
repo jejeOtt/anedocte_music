173a13a3571255695dc51b805a71d8af28343ad8
jejeOtt <48128613+jejeOtt@users.noreply.github.com>
2021-01-06 15:43:32 +0100

    Update tracks.php

--- application/views/back_office/tracks.php
+++ application/views/back_office/tracks.php
@@ -4,27 +4,27 @@
 <?php if ($tracks) : ?>
     <?php foreach ($tracks as $track) : ?>
         <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"> Titre du morceau : <?php echo $track['nameTrack']; ?></h5><br>
-                        <p class="card-text">Consulter les détails de ce morceau ici : </p><br>
-                        <p style="text-align:left"><a class="btn btn-lg btn-warning" href="<?php echo site_url('/tracks/' . $track['slug']); ?>">Detail</a> <a class="btn btn-lg btn-info" href="<?php echo site_url('/back_office/validate_track/' . $track['idTrack']); ?>">Valider la track</a></p><br>
-                        <p class="card-text">Track postée le : <?php echo $track['createdAt']; ?>
-                        <p></p>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title"> Titre du morceau : <?php echo $track['nameTrack']; ?></h5><br>
+                                <p class="card-text">Track postée le : <?php echo $track['createdAt']; ?>
+                                <br>
+                                <p style="text-align:left"><a class="btn btn-lg btn-warning" href="<?php echo site_url('/tracks/' . $track['slug']); ?>">Detail</a></p>
+                                <br>  
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Video</h5>
+                                <p class="card-text">Vous pouvez écoutez ce morceau ici : </p>
+                                <iframe width="500" height="175" src="https://www.youtube.com/embed/<?php echo (substr($track["url"], 17)); ?>?autoplay=1&autohide=1&controls=1&showinfo=0&modestbranding=1&rel=0"></iframe>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Video</h5>
-                        <p class="card-text">Vous pouvez écoutez ce morceau ici : </p>
-                        <iframe width="500" height="175" src="https://www.youtube.com/embed/<?php echo $track["url"]; ?>?autoplay=1&autohide=1&controls=1&showinfo=0&modestbranding=1&rel=0"></iframe>
-                    </div>
-                </div>
-            </div>
-        </div>
         <br>
         <hr style="height:2px;border-width:0;color:gray;background-color:gray"><br>
     <?php endforeach; ?>
